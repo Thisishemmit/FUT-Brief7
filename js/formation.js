b1092c4716db8f6f97aefce3ea44353fd60fd0be
Mohamed Karroumi <mkaroumi123@gmail.com>
2024-12-01 17:59:24 +0100

    MK-22 refactor: simplify formation data structure: - organised formation objects to only include name of formation and the list of card positions

--- js/formation.js
+++ js/formation.js
@@ -1,77 +1,71 @@
 let selectedFormation = {
-    forward: [],
-    midfield: [],
-    defense: [],
-    goalkeeper: [],
-    formation: "4-3-3"
+    formation: "4-3-3",
+    prototype: []
 };
 
 const formations = [
     {
         formation: "4-4-2",
-        forward: 2,
-        midfield: 4,
-        defense: 4,
-        goalkeeper: 1,
-        prototype: {
-            forward: [{ pos: 'ST', x: 35, y: 30 }, { pos: 'ST', x: 65, y: 30 }],
-            midfield: [{ pos: 'LM', x: 15, y: 45 }, { pos: 'CM', x: 35, y: 50 }, { pos: 'CM', x: 65, y: 50 }, { pos: 'RM', x: 85, y: 45 }],
-            defense: [{ pos: 'LB', x: 20, y: 70 }, { pos: 'CB', x: 40, y: 75 }, { pos: 'CB', x: 60, y: 75 }, { pos: 'RB', x: 80, y: 70 }],
-            goalkeeper: [{ pos: 'GK', x: 50, y: 95 }]
-        }
+        prototype: [
+            { pos: 'ST', x: 35, y: 30 },
+            { pos: 'ST', x: 65, y: 30 },
+            { pos: 'LM', x: 15, y: 45 },
+            { pos: 'CM', x: 35, y: 50 },
+            { pos: 'CM', x: 65, y: 50 },
+            { pos: 'RM', x: 85, y: 45 },
+            { pos: 'LB', x: 20, y: 70 },
+            { pos: 'CB', x: 40, y: 75 },
+            { pos: 'CB', x: 60, y: 75 },
+            { pos: 'RB', x: 80, y: 70 },
+            { pos: 'GK', x: 50, y: 95 }
+        ]
     },
     {
         formation: "4-3-3",
-        forward: 3,
-        midfield: 3,
-        defense: 4,
-        goalkeeper: 1,
-        prototype: {
-            forward: [{ pos: 'LW', x: 20, y: 30 }, { pos: 'st', x: 50, y: 25 }, { pos: 'RW', x: 80, y: 30 }],
-            midfield: [{ pos: 'CM', x: 30, y: 55 }, { pos: 'CM', x: 50, y: 50 }, { pos: 'CM', x: 70, y: 55 }],
-            defense: [{ pos: 'LB', x: 20, y: 70 }, { pos: 'CB', x: 40, y: 75 }, { pos: 'CB', x: 60, y: 75 }, { pos: 'RB', x: 80, y: 70 }],
-            goalkeeper: [{ pos: 'GK', x: 50, y: 95 }]
-        }
+        prototype: [
+            { pos: 'LW', x: 20, y: 30 },
+            { pos: 'ST', x: 50, y: 25 },
+            { pos: 'RW', x: 80, y: 30 },
+            { pos: 'CM', x: 30, y: 55 },
+            { pos: 'CM', x: 50, y: 50 },
+            { pos: 'CM', x: 70, y: 55 },
+            { pos: 'LB', x: 20, y: 70 },
+            { pos: 'CB', x: 40, y: 75 },
+            { pos: 'CB', x: 60, y: 75 },
+            { pos: 'RB', x: 80, y: 70 },
+            { pos: 'GK', x: 50, y: 95 }
+        ]
     },
     {
         formation: "4-5-1",
-        forward: 1,
-        midfield: 5,
-        defense: 4,
-        goalkeeper: 1,
-        prototype: {
-            forward: [{ pos: 'st', x: 50, y: 30 }],
-            midfield: [{ pos: 'cm', x: 20, y: 50 }, { pos: 'cm', x: 30, y: 50 }, { pos: 'cm', x: 40, y: 50 }, { pos: 'cm', x: 60, y: 50 }, { pos: 'cm', x: 70, y: 50 }],
-            defense: [{ pos: 'cb', x: 20, y: 70 }, { pos: 'cb', x: 40, y: 70 }, { pos: 'cb', x: 60, y: 70 }, { pos: 'cb', x: 80, y: 70 }],
-            goalkeeper: [{ pos: 'gk', x: 50, y: 95 }]
-        }
+        prototype: [
+            { pos: 'ST', x: 50, y: 30 },
+            { pos: 'LM', x: 20, y: 50 },
+            { pos: 'CM', x: 35, y: 50 },
+            { pos: 'CM', x: 50, y: 45 },
+            { pos: 'CM', x: 65, y: 50 },
+            { pos: 'RM', x: 80, y: 50 },
+            { pos: 'LB', x: 20, y: 70 },
+            { pos: 'CB', x: 40, y: 75 },
+            { pos: 'CB', x: 60, y: 75 },
+            { pos: 'RB', x: 80, y: 70 },
+            { pos: 'GK', x: 50, y: 95 }
+        ]
     }
-]
-
-
-// atahtaj players o formation bash tht l players f formation
-function populateFormation() {
-}
-
+];
 
 function updateCardsPosition() {
     let cards = document.querySelectorAll('.player-card');
-    let formationLines = Object.keys(formations.find(f => f.formation === selectedFormation.formation).prototype);
-    let cardIndex = 0;
-    for (let lines = 0; lines < 4; lines++) {
-        const line = formationLines[lines];
-        const prototypeLine = formations.find(f => f.formation === selectedFormation.formation).prototype[line];
-        for (let i = 0; i < prototypeLine.length; i++) {
-            const prototypePlayer = prototypeLine[i];
-            let x = prototypePlayer.x;
-            let y = prototypePlayer.y;
-            let card = cards[cardIndex];
-            card.style.left = `${x}%`;
-            card.style.top = `${y}%`;
+    let formation = formations.find(f => f.formation === selectedFormation.formation);
+    
+    formation.prototype.forEach((position, index) => {
+        if (index < cards.length) {
+            let card = cards[index];
+            card.style.left = `${position.x}%`;
+            card.style.top = `${position.y}%`;
             card.style.transform = 'translateY(-100%) translateX(-50%)';
-            cardIndex++;
         }
-    }
+    });
 }
 
 updateCardsPosition();
