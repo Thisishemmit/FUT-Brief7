842406394fd6faee430e3cb2ca4e4cbb7dfd5a74
Mohamed Karroumi <mkaroumi123@gmail.com>
2024-12-03 20:48:12 +0100

    MK-44 added the chemistrybdisplay component to html with nice ui; updated the formation select with same ui asthetic;

---
+++ js/chemistry.js
@@ -0,0 +1,68 @@
+
+function calculatePlayerChemistryPsition(formation, fieldPlayer) {
+    const playerPosition = formation.prototype[fieldPlayer.cardId].pos === fieldPlayer.player.position;
+    return playerPosition ? 10 : 0;
+}
+
+function calculatePlayerChemistryClub(fieldPlayer, fieldPlayers) {
+    const playerClub = fieldPlayer.player.club;
+    const clubLinks = fieldPlayers.filter(fp => fp.player.club === playerClub).length;
+    return clubLinks > 1 ? 3 : 0;
+}
+
+function calculatePlayerChemistryLeague(formation, fieldPlayer, fieldPlayers) {
+    const playerLeague = fieldPlayer.player.league;
+    const allLinks = []
+    formation.links.forEach(link => {
+        if (link.includes(fieldPlayer.cardId)) {
+            allLinks.push(link);
+        }
+    });
+    let adjacentPlayers = [];
+
+    allLinks.forEach(link => {
+        link.forEach(pos => {
+            if (pos !== fieldPlayer.cardId) {
+                let fp = fieldPlayers.find(fp => fp.cardId === pos);
+                if (fp) {
+                    adjacentPlayers.push(fp);
+                }
+            }
+        });
+    });
+
+    if (adjacentPlayers.length === 0) {
+        return 0;
+    }
+
+    console.log('adjacent players', adjacentPlayers);
+    
+    const leagueLinks = adjacentPlayers.filter(ap => ap.player.league === playerLeague).length;
+    return Math.min(leagueLinks, 2) * 2;
+}
+
+function calculatePlayerChemistryNationality(fieldPlayer, fieldPlayers) {
+    const allNationalities = fieldPlayers.map(fp => fp.player.nationality);
+    return allNationalities.filter(n => n === fieldPlayer.player.nationality).length > 1 ? 1 : 0;
+}
+
+function calculatePlayersChemistry(formation, fieldPlayers) {
+    if (fieldPlayers.length === 0) {
+        return 0;
+    }
+    return fieldPlayers.reduce((total, fieldPlayer) => {
+        const position = calculatePlayerChemistryPsition(formation, fieldPlayer);
+        console.log('for player', fieldPlayer.player.firstName, fieldPlayer.player.lastName, 'position', position);
+        
+        const club = calculatePlayerChemistryClub(fieldPlayer, fieldPlayers);
+        console.log('for player', fieldPlayer.player.firstName, fieldPlayer.player.lastName, 'club', club);
+        
+        const league = calculatePlayerChemistryLeague(formation, fieldPlayer, fieldPlayers);
+        console.log('for player', fieldPlayer.player.firstName, fieldPlayer.player.lastName, 'league', league);
+        
+        const nationality = calculatePlayerChemistryNationality(fieldPlayer, fieldPlayers);
+        console.log('for player', fieldPlayer.player.firstName, fieldPlayer.player.lastName, 'nationality', nationality);
+        
+        return total + position + club + league + nationality;
+    }, 0);
+}
